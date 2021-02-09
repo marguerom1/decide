7b0136ae2d1e68a87c8c6744c7910fc4b7346458
marguerom1 <martinguerreroromero@gmail.com>
2021-02-09 16:09:17 +0000

    settings fixed

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -165,6 +165,20 @@ KEYBITS = 256
 ALLOWED_VERSIONS = ['v1', 'v2']
 DEFAULT_VERSION = 'v1'
 
+BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
+}
+
 try:
     from local_settings import *
 except ImportError:
@@ -180,3 +194,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
